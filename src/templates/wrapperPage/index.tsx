3d94717c29fc05f35d16fe3cf9062ac93f65ef66
Erickson Manuel Holguin <erickson01d@gmail.com>
2022-04-07 11:34:19 -0400

    Adding seo structure and favicon

--- src/templates/wrapperPage/index.tsx
+++ src/templates/wrapperPage/index.tsx
@@ -1,21 +1,28 @@
 import React, { FC } from "react";
 
 import { Box, SimpleGrid, Stack } from "@chakra-ui/react";
+
+import { NextSeo } from "next-seo";
+
 import { Header } from "../header";
 
 interface WrapperPageProps {
   title: string;
+  description?: string;
 }
 
-export const WrapperPage: FC<WrapperPageProps> = ({ children }) => {
+export const WrapperPage: FC<WrapperPageProps> = ({ children, title, description }) => {
   return (
-    <Stack bgColor="#eceff3" h="100vh">
-      <Header />
-      <Box px="2">
-        <SimpleGrid columns={12} gap={3}>
-          {children}
-        </SimpleGrid>
-      </Box>
-    </Stack>
+    <>
+      <NextSeo title={`${title} | Alerta Ciudadana`} description={description} />
+      <Stack bgColor="#eceff3" h="100vh">
+        <Header />
+        <Box px="2">
+          <SimpleGrid columns={12} gap={3}>
+            {children}
+          </SimpleGrid>
+        </Box>
+      </Stack>
+    </>
   );
 };
