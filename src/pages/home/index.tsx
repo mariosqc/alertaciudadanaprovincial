677e785a29bc0ef8a2ce89ee6b8d53f1afc2e871
Erickson Manuel Holguin <erickson01d@gmail.com>
2022-04-07 08:23:54 -0400

    Adding colors schemas

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,13 +3,44 @@ import React from "react";
 import { NextPage } from "next";
 
 import { Button } from "@/components";
-import { Box, HStack, Text } from "@chakra-ui/react";
+import { Box, HStack, Stack, Text } from "@chakra-ui/react";
 
 export const HomePage: NextPage = () => {
   return (
-    <Box padding="25">
+    <Stack padding="25">
+      <HStack>
+        {/*  <Button colorScheme="red">Sí, Eliminar usuario</Button>
+      <Button colorScheme="pri">Sí, Eliminar usuario</Button>
+      <Button colorScheme="blue">Saludo</Button>
+      <Button colorScheme="green">Saludo</Button>
+      <Button colorScheme="purple">Saludo</Button> */}
+        <Box w="20" h="20" bgColor="pri.100" />
+        <Box w="20" h="20" bgColor="pri.200" />
+        <Box w="20" h="20" bgColor="pri.300" />
+        <Box w="20" h="20" bgColor="pri.400" />
+        <Box w="20" h="20" bgColor="pri.500" />
+        <Box w="20" h="20" bgColor="pri.600" />
+        <Box w="20" h="20" bgColor="pri.700" />
+        <Box w="20" h="20" bgColor="pri.800" />
+        <Box w="20" h="20" bgColor="pri.900" />
+      </HStack>
+      <HStack>
+        <Box w="20" h="20" bgColor="sec.100" />
+        <Box w="20" h="20" bgColor="sec.200" />
+        <Box w="20" h="20" bgColor="sec.300" />
+        <Box w="20" h="20" bgColor="sec.400" />
+        <Box w="20" h="20" bgColor="sec.500" />
+        <Box w="20" h="20" bgColor="sec.600" />
+        <Box w="20" h="20" bgColor="sec.700" />
+        <Box w="20" h="20" bgColor="sec.800" />
+        <Box w="20" h="20" bgColor="sec.900" />
+      </HStack>
       <HStack>
         <Button colorScheme="red">Sí, Eliminar usuario</Button>
+        <Button colorScheme="pri" variant="outline">
+          Sí, Eliminar usuario
+        </Button>
+        <Button colorScheme="sec">Sí, Eliminar usuario</Button>
         <Button colorScheme="blue">Saludo</Button>
         <Button colorScheme="green">Saludo</Button>
         <Button colorScheme="purple">Saludo</Button>
@@ -20,6 +51,6 @@ export const HomePage: NextPage = () => {
         ullam fugiat ducimus! Cupiditate error sunt veritatis vero reprehenderit
         ut natus praesentium!
       </Text>
-    </Box>
+    </Stack>
   );
 };
--- src/themes/original/theme.ts
+++ src/themes/original/theme.ts
@@ -1,5 +1,8 @@
 import { extendTheme } from "@chakra-ui/react";
 
+import { colors } from "./colors";
+
 export const theme = extendTheme({
   fonts: { heading: "Open Sans, sans-serif" },
+  colors,
 });
