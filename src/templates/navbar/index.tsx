db56c72e30ab0652707c1436314891cdc22c16a5
Erickson Manuel Holguin <erickson01d@gmail.com>
2022-04-07 12:07:00 -0400

    Include all routers

--- src/templates/navbar/index.tsx
+++ src/templates/navbar/index.tsx
@@ -3,54 +3,35 @@ import { useRouter } from "next/router";
 import React, { useState } from "react";
 
 export const Navbar = () => {
-  const { asPath, push } = useRouter();
+  const { pathname, push } = useRouter();
   const [items] = useState([
-    {
-      label: "Dashboard",
-      pathname: "/dashboard",
-    },
-    {
-      label: "Usuarios",
-      pathname: "/users",
-    },
-    {
-      label: "Seguimiento",
-      pathname: "/tracker",
-    },
-    {
-      label: "Emergencias",
-      pathname: "/emergencies",
-    },
-    {
-      label: "Denuncias",
-      pathname: "/complaints",
-    },
+    { label: "Dashboard", pathname: "/dashboard" },
+    { label: "Usuarios", pathname: "/users" },
+    { label: "Seguimiento", pathname: "/tracker" },
+    { label: "Emergencias", pathname: "/emergencies" },
+    { label: "Denuncias", pathname: "/complaints" },
   ]);
 
   return (
     <div>
       <HStack>
-        {items.map((item, i) => {
-          const isActive = asPath === item.pathname;
-
-          return (
-            <Box
-              key={i}
-              cursor="pointer"
-              _hover={{ bgColor: "pri.50", color: "pri.500" }}
-              borderBottomWidth="4px"
-              borderBottomColor={isActive ? "pri.600" : "transparent"}
-              px="2"
-              userSelect="none"
-              color={isActive ? "pri.600" : "black"}
-              fontWeight="medium"
-              py="4"
-              onClick={() => push(item.pathname)}
-            >
-              {item.label}
-            </Box>
-          );
-        })}
+        {items.map((item, i) => (
+          <Box
+            key={i}
+            cursor="pointer"
+            _hover={{ bgColor: "pri.50", color: "pri.500" }}
+            borderBottomWidth="4px"
+            borderBottomColor={pathname.includes(item.pathname) ? "pri.600" : "transparent"}
+            px="2"
+            userSelect="none"
+            color={pathname.includes(item.pathname) ? "pri.600" : "black"}
+            fontWeight="medium"
+            py="4"
+            onClick={() => push(item.pathname)}
+          >
+            {item.label}
+          </Box>
+        ))}
       </HStack>
     </div>
   );
