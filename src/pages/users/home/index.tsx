01d95df991b5cd0f6ee31c4729a1b1d018d423b3
Erickson Manuel Holguin <erickson01d@gmail.com>
2022-04-08 12:07:25 -0400

    Adding wrapper cards

--- src/pages/users/home/index.tsx
+++ src/pages/users/home/index.tsx
@@ -2,7 +2,17 @@ import React from "react";
 
 import { NextPage } from "next";
 import { WrapperPage } from "@/templates";
+import { Card } from "@/layout";
 
 export const UsersPage: NextPage = () => {
-  return <WrapperPage title="Usuarios">UsersPage</WrapperPage>;
+  return (
+    <WrapperPage title="Usuarios">
+      <Card.Wrapper colSpan={12}>
+        <Card.Header title="Listado de Usuarios" />
+        <Card.Body>
+          EmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPage
+        </Card.Body>
+      </Card.Wrapper>
+    </WrapperPage>
+  );
 };
