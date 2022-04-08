3a3148866cc6eedb5a638795c8b3699e16ec7575
Erickson Manuel Holguin <erickson01d@gmail.com>
2022-04-08 12:23:43 -0400

    Adding icons in header

--- src/templates/header/index.tsx
+++ src/templates/header/index.tsx
@@ -1,17 +1,41 @@
 import { Card } from "@/layout";
-import { Flex, Text } from "@chakra-ui/react";
+import { Avatar, Flex, HStack, IconButton, Text } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 import React from "react";
+import { Bell, Settings } from "react-feather";
 import { Navbar } from "../navbar";
 
 export const Header = () => {
+  const { push } = useRouter();
+
   return (
     <div>
       <Card.Wrapper rounded="none" shadow="sm">
-        <Flex px="6">
+        <Flex px="6" justifyContent="space-between">
           <Flex>
             <Text mr="12">Titulo aqui</Text>
             <Navbar />
           </Flex>
+          <HStack>
+            <IconButton
+              aria-label="Settings"
+              size="sm"
+              variant="ghost"
+              _focus={{}}
+              colorScheme="blue"
+              icon={<Settings size="1.25rem" />}
+              onClick={() => push("/settings")}
+            />
+            <IconButton
+              aria-label="Settings"
+              size="sm"
+              variant="ghost"
+              _focus={{}}
+              colorScheme="green"
+              icon={<Bell size="1.25rem" />}
+            />
+            <Avatar size="sm" />
+          </HStack>
         </Flex>
       </Card.Wrapper>
     </div>
