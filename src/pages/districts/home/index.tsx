fb11b17f26eee7fbdad5bf95e1bf1f9b5ab84ee5
Erickson Manuel Holguin <erickson01d@gmail.com>
2022-04-09 15:10:25 -0400

    Adding card for districts

--- src/pages/districts/home/index.tsx
+++ src/pages/districts/home/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import { NextPage } from "next";
 import { WrapperPage } from "@/templates";
+import { Card } from "@/layout";
 
 export const DistrictTemplate: NextPage = () => {
   return (
     <WrapperPage title="Distritos" breadcrumb={{ routes: ["districts"] }}>
-      DistrictTemplate
+      <Card.Wrapper colSpan={12}>
+        <Card.Header title="Listado de distritos" />
+      </Card.Wrapper>
     </WrapperPage>
   );
 };
