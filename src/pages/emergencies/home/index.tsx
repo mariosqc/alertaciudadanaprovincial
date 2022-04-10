20cfb4e7549ff0a962bf0b2bc8f77354e6b162f3
Erickson Manuel Holguin <erickson01d@gmail.com>
2022-04-10 07:52:43 -0400

    Adding mapa to emegencies screen

--- src/pages/emergencies/home/index.tsx
+++ src/pages/emergencies/home/index.tsx
@@ -4,15 +4,15 @@ import { NextPage } from "next";
 import { WrapperPage } from "@/templates";
 import { Card } from "@/layout";
 import { Box, Flex, GridItem, Heading, HStack } from "@chakra-ui/react";
-import { Button } from "@/components";
+import { Button, Map } from "@/components";
 
 export const EmergenciesPage: NextPage = () => {
   return (
-    <WrapperPage title="Emergencias" breadcrumb={{ routes: ["emergencies"] }}>
+    <WrapperPage fullScreen title="Emergencias" breadcrumb={{ routes: ["emergencies"] }}>
       <Card.Wrapper colSpan={12}>
         <Card.Header title="Listado de Emergencias" />
-        <Card.Body>
-          EmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPageEmergenciesPage
+        <Card.Body h="95%">
+          <Map.Map />
         </Card.Body>
       </Card.Wrapper>
     </WrapperPage>
