e5d6709d6fc1073ef8fb42c58b749a81ca453217
Erickson Manuel Holguin <erickson01d@gmail.com>
2022-04-15 09:01:28 -0400

    Map for districts

---
+++ src/pages/districts/home/MapForDistricts.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+import { Card } from "@/layout";
+import { Button, Map } from "@/components";
+
+export const MapForDistricts = () => {
+  const [isAddingDistric, setIsAddingDistric] = useState(false);
+
+  return (
+    <Card.Wrapper colSpan={12}>
+      <Card.Header
+        title="Listado de distritos"
+        optionsRight={[
+          <Button size="sm" colorScheme="pri" key="new" onClick={() => setIsAddingDistric(!isAddingDistric)}>
+            Nuevo Distrito
+          </Button>,
+        ]}
+      />
+      <Card.Body h="95%">
+        <Map.Map />
+      </Card.Body>
+    </Card.Wrapper>
+  );
+};
