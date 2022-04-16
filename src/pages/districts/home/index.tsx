ea2ce070793b5231cb7414ebffb20384f582c3bb
Erickson Manuel Holguin <erickson01d@gmail.com>
2022-04-16 16:55:28 -0400

    Adding subtitle

--- src/pages/districts/home/index.tsx
+++ src/pages/districts/home/index.tsx
@@ -10,7 +10,11 @@ export const DistrictTemplate: NextPage = () => {
   return (
     <WrapperPage title="Listado de Distritos" breadcrumb={{ routes: ["districts"] }}>
       <Card.Wrapper colSpan={12}>
-        <Card.Header title="Listado de Distritos" optionsRight={[<NewDistrict key={1} />]} />
+        <Card.Header
+          title="Listado de Distritos"
+          subtitle="25 Resultados encontrados"
+          optionsRight={[<NewDistrict key={1} />]}
+        />
         <Card.Body px="0">
           <DistrictTable />
         </Card.Body>
