f685d142e5cd4a7b1b7027c3ab3748f553a051ea
Erickson Manuel Holguin <erickson01d@gmail.com>
2022-04-16 16:20:50 -0400

    Avanced auth

--- pages/example.tsx
+++ pages/example.tsx
@@ -1,13 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-import { database } from "@/firebase";
+import { auth, database } from "@/firebase";
 import { Box } from "@chakra-ui/react";
+import { faker } from "@faker-js/faker";
 
 const ExamplePage = () => {
-  async function getData() {
-    const response = await database.ref("/admin/districts").get();
+  const [email, setEmail] = useState(faker.internet.email());
+  const [password, setPassword] = useState(faker.internet.password());
 
-    console.log(response.val());
+  async function getData() {
+    // const response = await auth.signInWithEmailAndPassword("Lafayette54@yahoo.com", "9VaS6q3zZxLAlwc");
+    // Lafayette54@yahoo.com 9VaS6q3zZxLAlwc
+    /*  const response = await auth.createUserWithEmailAndPassword(email, password); */
+
+    // Check if user is auth
+    const user = auth.currentUser;
+
+    console.log(user?.toJSON());
+
+    // Logout
+    // auth.signOut();
   }
 
   useEffect(() => {
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,7 +1,9 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/database";
+import "firebase/compat/auth";
 import { options } from "./firebase.json";
 
 firebase.initializeApp(options);
 
 export const database = firebase.database();
+export const auth = firebase.auth();
