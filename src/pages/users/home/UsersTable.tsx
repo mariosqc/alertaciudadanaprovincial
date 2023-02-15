36c81bab4b3d29983d27711c8ad323904257af78
Erickson Manuel Holguin <erickson01d@gmail.com>
2023-02-15 06:40:59 -0400

    Change table

--- src/pages/users/home/UsersTable.tsx
+++ src/pages/users/home/UsersTable.tsx
@@ -1,80 +1,67 @@
-import React from "react";
+import React, { useMemo } from "react";
 
-import { Avatar, Table, TableContainer, Tag, Tbody, Td, Text, Tfoot, Th, Thead, Tr } from "@chakra-ui/react";
+import { Avatar, Center, Tag, Text } from "@chakra-ui/react";
 
 import moment from "moment";
 import { useUserContext } from "@/contexts";
 import { UserModal } from "./UserModal";
 import { InactivateUser } from "./InactivateUser";
+import { User } from "@alerta-ciudadana/entity";
+import { createColumnHelper } from "@tanstack/react-table";
+import { Table } from "@/components";
+
+interface TableRowType {
+  isSelected: boolean;
+}
+
+interface TableDataType extends User, TableRowType {}
+
+const columnHelper = createColumnHelper<TableDataType>();
 
 export const UsersTable = () => {
   const { users } = useUserContext();
 
-  return (
-    <TableContainer>
-      <Table size="sm" variant="striped">
-        <Thead>
-          <Tr>
-            <Th w="0"></Th>
-            <Th>Nombre</Th>
-            <Th>Correo electrónico</Th>
-            <Th>Teléfono</Th>
-            <Th>Fecha de Creación</Th>
-            <Th>Sexo</Th>
-            <Th textAlign="center">Puntos</Th>
-            <Th w="0"></Th>
-            <Th w="0"></Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {users.length === 0 && (
-            <>
-              <Tr>
-                <Td colSpan={6}>
-                  <Text py="2" textAlign="center" fontWeight="semibold">
-                    No se han encontrado usuarios
-                  </Text>
-                </Td>
-              </Tr>{" "}
-            </>
-          )}
-          {users.map((user) => (
-            <Tr key={user.uid}>
-              <Td w="0">
-                <Avatar w="8" h="8" src={user.avatarUrl} />
-              </Td>
-              <Td>{user.name}</Td>
-              <Td>{user.email}</Td>
-              <Td>{user.phone}</Td>
-              <Td>
-                <Text lineHeight="none">{moment(user.date).format("LLL")}</Text>
-              </Td>
-              <Td>{user.sex}</Td>
-              <Td textAlign="center">
-                <Tag variant="solid" colorScheme="cyan">
-                  {user.points}
-                </Tag>
-              </Td>
-              <Td>
-                <InactivateUser user={user} />
-              </Td>
-              <Td>
-                <UserModal user={user} />
-              </Td>
-            </Tr>
-          ))}
-        </Tbody>
-        <Tfoot>
-          <Tr>
-            <Th w="0"></Th>
-            <Th>Nombre</Th>
-            <Th>Teléfono</Th>
-            <Th>Fecha de Creación</Th>
-            <Th>Sexo</Th>
-            <Th w="0"></Th>
-          </Tr>
-        </Tfoot>
-      </Table>
-    </TableContainer>
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor("avatarUrl", {
+        cell: ({ getValue }) => {
+          return <Avatar w="8" h="8" src={getValue()} />;
+        },
+        header: "Avatar",
+        enableColumnFilter: false,
+      }),
+      columnHelper.accessor("name", { cell: ({ getValue }) => getValue(), header: "Nombre" }),
+      columnHelper.accessor("email", { cell: ({ getValue }) => getValue(), header: "Correo electrónico" }),
+      columnHelper.accessor("phone", { cell: ({ getValue }) => getValue(), header: "Teléfono" }),
+      columnHelper.accessor("date", {
+        cell: ({ getValue }) => <Text lineHeight="none">{moment(getValue()).format("LLL")}</Text>,
+        header: "Fecha de Creación",
+      }),
+      columnHelper.accessor("sex", { cell: ({ getValue }) => getValue(), header: "Sexo" }),
+      columnHelper.accessor("points", {
+        cell: ({ getValue }) => (
+          <Center>
+            <Tag variant="solid" colorScheme="cyan">
+              {getValue()}
+            </Tag>
+          </Center>
+        ),
+        header: "Puntos",
+        enableColumnFilter: false,
+      }),
+      columnHelper.accessor("isSelected", {
+        cell: ({ row }) => <InactivateUser user={row.original} />,
+        header: "",
+        enableColumnFilter: false,
+      }),
+      columnHelper.accessor("imei", {
+        cell: ({ row }) => <UserModal user={row.original} />,
+        header: "",
+        enableColumnFilter: false,
+      }),
+    ],
+    []
   );
+
+  return <Table columns={columns} data={users as TableDataType[]} />;
 };
